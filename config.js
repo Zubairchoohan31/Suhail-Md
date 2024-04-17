0cef3baf179b7e0e95b483cd1ee6bd7eafe2438d
Zubairchoohan31 <160321525+Zubairchoohan31@users.noreply.github.com>
2024-04-17 19:34:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
 global.gurl  =process.env.GURL  || ""
 global.website=process.env.GURL || ""
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/f9a765eaad2d9786e0fba.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
